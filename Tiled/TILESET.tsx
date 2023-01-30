3b92616cb94bd1b5bdb5c93abb44de5c74cd7007
Boureux Nathan <nathan.boureux@etu.umontpellier.fr>
2023-01-30 16:27:17 +0100

    ajout des tiles

---
+++ Tiled/TILESET.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="TILESET" tilewidth="50" tileheight="50" tilecount="798" columns="38">
+ <image source="../Map_artwork_1.jpg" width="1920" height="1080"/>
+</tileset>
