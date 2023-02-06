cca00099770cd2a2adf9215fb8dcc4ed4700b699
Boureux Nathan <nathan.boureux@etu.umontpellier.fr>
2023-02-06 14:17:21 +0100

    sprite movement working

--- main.cpp
+++ main.cpp
@@ -1,54 +1,96 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <math.h>
 using namespace std;
 using namespace sf;
 
+#define SPRITE_SPEED 5
+
 int main()
 {
-    RenderWindow window(VideoMode(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height), "Body Guard");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Green);
+    RenderWindow window(VideoMode(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height), "Body Guard",Style::Fullscreen);
+    
+    window.setVerticalSyncEnabled (true);
+    window.setKeyRepeatEnabled(false);
 
-    while (window.isOpen())
-    {
-        Texture texture;
-        if (!texture.loadFromFile("dragon.png")){
+    // Create sprite and apply texture
+    Texture texture;
+        if (!texture.loadFromFile("Spritev1.png")){
             cerr << "failed to load image" << endl;
             exit(1);
         }
-        texture.setRepeated(false);
-        texture.setSmooth(true);
+    texture.setRepeated(false);
+    texture.setSmooth(true);
+    Sprite sprite;
+    sprite.setTexture(texture);
+    
+    // Sprite coordinates
+    int x=window.getSize().x/2.;
+    int y=window.getSize().y/2.;
 
-        Sprite sprite;
-        sprite.setTexture(texture);
+    // Flags for key pressed
+    bool upFlag=false;
+    bool downFlag=false;
+    bool leftFlag=false;
+    bool rightFlag=false;
 
+    while (window.isOpen())
+    {  
         Event event;
-        bool movespriteleft = false;
-        bool movespriteright = false;
         while (window.pollEvent(event))
         {
-            if ((event.type == Event::KeyPressed) && (event.key.code == Keyboard::Left))
-                movespriteleft = true;
-            if ((event.type == Event::KeyPressed) && (event.key.code == Keyboard::Right))
-                movespriteright = true;
-            if (event.type == Event::Closed)
-                window.close();
+            if (event.type == Event::Closed) window.close();
+
+            // If a key is pressed
+            if (event.type == Event::KeyPressed){
+                switch (event.key.code){
+                // If escape is pressed, close the application
+                case  sf::Keyboard::Escape : window.close(); break;
+
+                // up, down, left and right keys
+                case sf::Keyboard::Up :     upFlag=true; break;
+                case sf::Keyboard::Down:    downFlag=true; break;
+                case sf::Keyboard::Left:    leftFlag=true; break;
+                case sf::Keyboard::Right:   rightFlag=true; break;
+                default : break;
+                }
+            }
+
+            // If a key is released
+            if (event.type == sf::Event::KeyReleased)
+            {
+                switch (event.key.code)
+                {
+                // up, down, left and right keys
+                case sf::Keyboard::Up :     upFlag=false; break;
+                case sf::Keyboard::Down:    downFlag=false; break;
+                case sf::Keyboard::Left:    leftFlag=false; break;
+                case sf::Keyboard::Right:   rightFlag=false; break;
+                default : break;
+                }
+            }
         }
 
-        if (movespriteleft){
-            sprite.move(-10.f,-10.f);
-        }
-        if (movespriteright){
-            sprite.move(10.f,10.f);
-        }
-        // en gros c'est a peu près ça mais plus compliqué.
-        // voir https://lucidar.me/en/sfml/sfml-part-4-moving-a-sprite-with-the-keyboard/
+        // Update coordinates
+        if (leftFlag) x-=SPRITE_SPEED;
+        if (rightFlag) x+=SPRITE_SPEED;
+        if (upFlag) y-=SPRITE_SPEED;
+        if (downFlag) y+=SPRITE_SPEED;
 
-        window.clear();
-//        window.draw(shape);
+        // Check screen boundaries
+        if (x<0) x=0;
+        if (x>(int)window.getSize().x) x=window.getSize().x;
+        if (y<0) y=0;
+        if (y>(int)window.getSize().y) y=window.getSize().y;
+
+        // Clear the window and apply grey background
+        window.clear(sf::Color(127,127,127));
+
+        // Rotate and draw the sprite
+        sprite.setPosition(x,y);
         window.draw(sprite);
+
         window.display();
     }
-
     return 0;
 }
