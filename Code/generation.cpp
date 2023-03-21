8113e1a7c739e4de0aa927351366e967777aac39
Norazu <nokrani50@gmail.com>
2023-03-21 16:00:58 +0100

    commit final generation

--- Code/generation.cpp
+++ Code/generation.cpp
@@ -6,34 +6,32 @@ using namespace std;
 
 // Fonction récursive pour remplir la carte
 // Prend en entrée les coordonnées x et y de départ, et le nombre de salles restant à placer
-int fill_map(int x, int y, int* nb_salles, int mat[][8]){
+void fill_map(int x, int y, int* nb_salles, int mat[][8]){
     // Cas de base : si on a placé toutes les salles ou si on sort de la carte
     if(*nb_salles <= 0 || x < 0 || x >= 9 || y < 0 || y >= 8){
-        return 0;
+        return;
     }
     // On place une salle dans la case actuelle
     if(rand()%2 == 1){
         mat[x][y] = 1 ;
         (*nb_salles)-- ;
-    // On appelle récursivement fill_map pour chaque case voisine non explorée
-    if(x+1 < 9 && mat[x+1][y] == 0){
-        fill_map(x+1, y, nb_salles, mat);
+        // On appelle récursivement fill_map pour chaque case voisine non explorée
+        if(x+1 < 9 && mat[x+1][y] == 0){
+            fill_map(x+1, y, nb_salles, mat);
+        }
+        if(y+1 < 8 && mat[x][y+1] == 0){
+            fill_map(x, y+1, nb_salles, mat);
+        }
+        if(x-1 >= 0 && mat[x-1][y] == 0){
+            fill_map(x-1, y, nb_salles, mat);
+        }
+        if(y-1 >= 0 && mat[x][y-1] == 0){
+            fill_map(x, y-1, nb_salles, mat);
+        }
     }
-    if(y+1 < 8 && mat[x][y+1] == 0){
-        fill_map(x, y+1, nb_salles, mat);
-    }
-    if(x-1 >= 0 && mat[x-1][y] == 0){
-        fill_map(x-1, y, nb_salles, mat);
-    }
-    if(y-1 >= 0 && mat[x][y-1] == 0){
-        fill_map(x, y-1, nb_salles, mat);
-    }   
-    }
-
-    return 0;
 }
 
-int bossRoom(int mat[][8]){
+void bossRoom(int mat[][8]){
     int maxx = 0;
     int maxy = 0;
     float max_dist = 0;
@@ -46,14 +44,8 @@ int bossRoom(int mat[][8]){
                 maxy = y;
             }  
         }
-
     }
-    cout << "max x" << maxx << "\n" << "max y" << maxy <<"\n" << "mat[maxx][maxy]" << mat[maxx][maxy]<< "\n";
     mat[maxx][maxy] = 2;
-    return 0;
-
-
-
 }
 
 int main(){
